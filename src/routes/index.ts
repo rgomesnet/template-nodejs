9291ecd060b5dd84c1a631c3a7773c19802ced84
Renato Gomes <renato@rgomes.net>
2020-06-23 17:38:55 -0300

    Initial commit

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+// import { Router } from 'express';
+// import transactionRouter from './transaction.routes';
+
+// const routes = Router();
+
+// // routes.use('/operation_routes', transactionRouter);
+
+// export default routes;
---
+++ src/routes/operation.routes.ts
@@ -0,0 +1,11 @@
+// import { Router } from 'express';
+// import Service from '../services/Service';
+// import Repository from '../repositories/Repository';
+
+// const operationRouter = Router();
+
+// operationRouter.get('/', (request, response) => {
+
+// });
+
+export default operationRouter;
